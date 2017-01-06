005a56cfb4a946675852b70f1622fb83bb3c0499
Ryan H <hzhengran@163.com>
2017-01-06 13:47:42 +0800

    Use transform to move background image

--- src/js/app/mover.js
+++ src/js/app/mover.js
@@ -1,17 +1,17 @@
-define([], function(){
-    var move = function(elementListen, elementMove, strength){
-        elementListen.on('mousemove', function(e){
+define([], function () {
+    var move = function (elementListen, elementMove, strength) {
+        elementListen.on('mousemove', function (e) {
             var imgHeight = elementMove.height();
             var imgWidth = elementMove.width();
-            var centerX =  imgWidth/2;
-            var centerY = imgHeight/2;
-            var offsetX = -(e.pageX - centerX)/imgWidth*strength;
-            var offsetY = -(e.pageY - centerY)/imgHeight*strength;
-            //elementMove.css("transform",'translate(' +offsetX+"px, "+offsetY+"px)");
+            var centerX = imgWidth / 2;
+            var centerY = imgHeight / 2;
+            var offsetX = -(e.pageX - centerX) / imgWidth * strength;
+            var offsetY = -(e.pageY - centerY) / imgHeight * strength;
+            elementMove.css("transform", 'translate(' + offsetX + "px, " + offsetY + "px) scale(1.1)");
 
-            elementMove.css("background-position", offsetX+"px "+offsetY+"px");
-        });        
+            //elementMove.css("background-position", offsetX + "px " + offsetY + "px");
+        });
     };
 
-    return { move:move };
+    return { move: move };
 });
